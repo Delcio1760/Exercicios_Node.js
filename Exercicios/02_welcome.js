80416f8b4512600b94fab48e321daed4d2ca2f5f
Delcio1760 <delcioaugustocunha@gmail.com>
2026-03-30 01:34:19 +0100

    Adição de uma docstring para o segundo exercício

--- Exercicios/02_welcome.js
+++ Exercicios/02_welcome.js
@@ -1,3 +1,18 @@
+/**
+ * Exercise 2 - Environment variable greeting
+ *
+ * Reads the USER_NAME environment variable and displays a personalized greeting.
+ * If the variable is not set, displays a generic message to the visitor.
+ *
+ * Usage:
+ *   node 02_welcome.js
+ *   USER_NAME=Teresa node 02_welcome.js
+ *
+ * process.env         - object containing all environment variables of the system
+ * process.env.USER_NAME - reads the value of the USER_NAME environment variable
+ *                         returns undefined if the variable is not set
+ */
+
 if(process.env.USER_NAME){
    
     console.log(`Hello ${process.env.USER_NAME}!Welcome to Node`)
